517e6b52eea2e7e2a592237bcf7924146debfac2
Raoufnig <aboubakar.abdoul@institutsaintjean.org>
2024-01-31 16:31:39 +0100

    Ajout des pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { ProfilComponent } from './views/profil/profil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: HomeComponent},
+    {path:'login', component: LoginComponent},
+    {path:'profil', component: ProfilComponent},
+
+];
+
