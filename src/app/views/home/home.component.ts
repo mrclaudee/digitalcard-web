a9c015f30aff1282f1aaeff3abbd2a8639a3618a
emmanuelle <sokengfemmanuellec@gmail.com>
2024-02-23 11:28:41 +0100

    creation du footer et de la page de d'acceuil

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ 
+    MatToolbarModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+    nom= `Pierre Durant`;
+    uniterecherche =`NETEC`
+    defunit=`Neuroépidémiologie Tropicale et Comparée`
+    profession1 = `Maître de Conférence des Universités`;
+    profession2 = `Praticien Hospitalier`;
 
 }
